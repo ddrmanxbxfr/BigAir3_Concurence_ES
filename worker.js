c873f43f31ab460daaa13045bcfe591b872493dc
Mathieu Rhéaume <mathieu@codingrhemes.com>
2014-04-26 13:13:53 -0400

    Multi thread basic

---
+++ worker.js
@@ -0,0 +1,11 @@
+onmessage = function(e){
+  if ( e.data === "start" ) {
+    // Do some computation
+    done()
+  }
+};
+ 
+function done(){
+  // Send back the results to the parent page
+  postMessage("done");
+}
